83bed8e1ae2091c253cf53408e85b87abf299185
Zachary Imbriaco <zachary@imbriaco.com>
2021-12-15 11:06:06 -0500

    Complete exercise 34.

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,19 @@
+# declare animals array
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The second animal is at 1 and is #{animals[1]}."
+puts "The animal at 1 is the 2nd animal and is #{animals[1]}."
+puts "The third animal is at 2 and is #{animals[2]}."
+puts "The animal at 2 is the 3rd animal and is #{animals[2]}."
+puts "The first animal is at 0 and is #{animals[0]}."
+puts "The animal at 0 is the 1st animal and is #{animals[0]}."
+puts "The fourth animal is at 3 and is #{animals[3]}."
+puts "The animal at 3 is the 4th animal and is #{animals[3]}."
+puts "The fifth animal is at 4 and is #{animals[4]}."
+puts "The animal at 4 is the 5th animal and is #{animals[4]}."
+puts "The third animal is at 2 and is #{animals[2]}."
+puts "The animal at 2 is the 3rd animal and is #{animals[2]}."
+puts "The sixth animal is at 5 and is #{animals[5]}."
+puts "The animal at 5 is the 6th animal and is #{animals[5]}."
+puts "The fifth animal is at 4 and is #{animals[4]}."
+puts "The animal at 4 is the 5th animal and is #{animals[4]}."
