27a483e277d24f66e883b96f7ef9fe696610f986
Zachary Imbriaco <zachary@imbriaco.com>
2021-12-15 12:22:18 -0500

    Complete hashes.rb exercise.

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,26 +8,30 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {zebras: 5, gorillas: 14, pandas: 12}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the zoo variable 
+p zoo.keys
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
 # YOUR CODE HERE
+p zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
 # YOUR CODE HERE
+p zoo[0]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
+zoo[:birds] = 194
+p zoo
 
-
+puts '-' * 20
 #-------------------
 # Part 2: Email
 #-------------------
@@ -38,19 +42,28 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+email = {
+  sender: "zachary@imbriaco.com",
+  recipient: "lorem@ipsum.com",
+  title: "Regarding placeholder text",
+  has_attachments: false,
+  sent_date: '12/15/2021'
+}
 
 # Write code that prints your email hash to the terminal.
+p email
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
+p email.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
+p email.values
 
-
+puts '-' * 20
 #-------------------
 # Part 3: Many Emails - OPTIONAL EXTENSION
 #-------------------
@@ -64,7 +77,7 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
 
@@ -76,7 +89,7 @@ posts = [
     'timestamp' => "4:37 PM August 13, 2019",
     'number_likes' => 0,
     'comments' => []
-  }, 
+  },
   {
     'image_src' => "./images/holiday-party.png",
     'caption' => "What a great holiday party omg",
@@ -90,12 +103,36 @@ puts posts
 puts posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
+emails = [
+  {
+    sender: "zachary@imbriaco.com",
+    recipient: "lorem@ipsum.com",
+    title: "Regarding placeholder text",
+    has_attachments: false,
+    sent_date: '12/15/2021'
+  },
+  {
+    sender: "zachary@imbriaco.com",
+    recipient: "lorem@ipsum.com",
+    title: "Regarding placeholder text",
+    has_attachments: false,
+    sent_date: '12/15/2021'
+  },
+  {
+    sender: "zachary@imbriaco.com",
+    recipient: "lorem@ipsum.com",
+    title: "Regarding placeholder text",
+    has_attachments: false,
+    sent_date: '12/15/2021'
+  }
+]
+p emails
